fd1b14cb0401efd77950a41d85500a24a5180b34
antomin <anton.mindlin@gmail.com>
2022-12-20 22:01:42 +0100

    pre release

--- client/main.js
+++ client/main.js
@@ -1,11 +1,28 @@
-const inputTextElem = document.getElementById('input-text');
-const inputBtnElem = document.getElementById('input-btn');
-const msgListElem = document.getElementById('messages-list');
+"use strict";
+
+const inputTextElem = document.getElementById("input-text");
+const inputBtnElem = document.getElementById("input-btn");
+const msgListElem = document.getElementById("messages-list");
+
+let ws = new WebSocket("ws://localhost:8000/ws");
 
 inputBtnElem.onclick = (event) => {
     event.preventDefault();
-    let newLiElem = document.createElement('li');
-    newLiElem.innerText = inputTextElem.value;
-    msgListElem.appendChild(newLiElem)
-    inputTextElem.value = '';
+    let data = JSON.stringify({
+        "action": "echo_send",
+        "message": inputTextElem.value
+    });
+
+    ws.send(data);
+
+    inputTextElem.value = "";
+}
+
+ws.onmessage = (event) => {
+    let data = JSON.parse(event.data);
+    if (data["action"] === "answer") {
+        let newLiElem = document.createElement("li");
+        newLiElem.innerText = data["num"] + '. ' + data["message"];
+        msgListElem.appendChild(newLiElem);
+    }
 }
--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, WebSocket
+import json
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.websocket('/ws')
+async def root(ws: WebSocket):
+    await ws.accept()
+    cnt = 1
+    while True:
+        rec_data = await ws.receive_json()
+        if rec_data['action'] == 'echo_send':
+            send_data = {
+                'action': 'answer',
+                'num': cnt,
+                'message': rec_data['message']
+            }
+            await ws.send_json(send_data)
+            cnt += 1
